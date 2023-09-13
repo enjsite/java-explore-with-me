09f77fb3fb026232dcded46fb6876f35c14831f2
enjsite <enjyweb@gmail.com>
2023-09-13 16:27:18 +0300

    Graduate: Stats module V1, fix by review

--- stats-service/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/EndpointHitDto.java
@@ -3,6 +3,7 @@ package ru.practicum;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
@@ -10,29 +11,26 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
-@Getter
-@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class EndpointHitDto {
 
-    private Integer id;
-
-    @NotNull
-    @NotBlank
-    private String app;
+    Integer id;
 
     @NotBlank
-    private String uri;
+    String app;
 
-    @NotNull
+    @NotBlank
+    String uri;
+
     @NotBlank
     @Length(min = 7, max = 15)
-    private String ip;
+    String ip;
 
     @NotNull
     @JsonProperty("timestamp")
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime timestamp;
+    LocalDateTime timestamp;
 }
--- stats-service/stats-dto/src/main/java/ru/practicum/ViewStatsDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/ViewStatsDto.java
@@ -1,22 +1,25 @@
 package ru.practicum;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ViewStatsDto {
 
     @NotBlank
-    private String app;
+    String app;
 
     @NotBlank
-    private String uri;
+    String uri;
 
     @NotBlank
-    private Long hits;
+    Long hits;
 }
